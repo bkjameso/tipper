1dc0c3d8409c1bc780aaefd87a611bce57e6d845
bkjameso <bjameson74@gmail.com>
2024-06-05 17:30:55 -0500

    added if statments

---
+++ main.py
@@ -0,0 +1,39 @@
+def tipper():
+    #This does the math for the tip
+    print("_____WELCOME TO TIPPER____")
+
+    amount_paid = float(input("how much did you pay?"))
+    great = amount_paid * 0.3
+    good = amount_paid * 0.25
+    okay = amount_paid * 0.15
+    bad = amount_paid * 0.05
+
+    #finds tip due to waiter performance
+    print("input great, good, okay, or bad please")
+    waiter_performance = input("How was your service?")
+    if waiter_performance == "great":
+        print(f"I reccommend tipping ${great:.2f}")
+    
+    elif waiter_performance == "good":
+        print(f"I reccommend tipping ${good:.2f}")
+    
+    elif waiter_performance == "okay":
+        print(f"I reccommend tipping ${okay:.2f}")
+    
+    elif waiter_performance == "bad":
+        print(f"I reccomend tipping ${bad:.2f}")
+    
+    else: print("Invalid input. Please input geat, good, okay, or bad")
+
+
+
+    print("___THANK YOU FOR USING TIPPER")
+
+tipper()
+
+
+
+    
+
+
+    
